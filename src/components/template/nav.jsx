85da02819b7f5304e8982f2ce48bfc58d93987b7
Cristiane Nazareth <cristianenazareth24@gmail.com>
2023-05-17 20:48:52 -0300

    identification bar

---
+++ src/components/template/nav.jsx
@@ -0,0 +1,16 @@
+import './nav.css'
+import React from 'react'
+
+
+export const Nav = pros => 
+
+    <nav className='nav'>
+        <div className="nav_nav"></div>
+        <div className='nav_navegacao'>
+            <img src="../assets/images/carta.png" alt="carta" />
+            <a className="nav_contat" href="https://scaweb.saude.gov.br/scaweb/acessoMenu.do?acao=listaEmailGestor">Contato</a>
+            &nbsp;&nbsp;1.0.6.3
+        </div>
+        
+    </nav>
+
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -5,6 +5,7 @@ import {Header} from '../components/template/header'
 import {Logo} from '../components/template/logo'
 import {Main} from '../components/template/main'
 import {Footer} from '../components/template/footer'
+import {Nav} from '../components/template/nav'
 
 
 
@@ -12,6 +13,7 @@ export const App = props => (
     // <h2>Teste cris</h2>
     <div className='app'>
         <Header></Header>
+        <Nav></Nav>
         <Main></Main>
         <Logo></Logo>
         <Footer></Footer>
