05db17271a1a6bb968eae243299f84f67107ddfc
Cristiane Nazareth <cristianenazareth24@gmail.com>
2023-05-19 11:14:51 -0300

    identification bar update

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,7 +4,6 @@ import React from 'react'
 import {Header} from '../components/template/header'
 import {Main} from '../components/template/main'
 import {Footer} from '../components/template/footer'
-import {Nav} from '../components/template/nav'
 
 
 
@@ -12,7 +11,6 @@ export const App = props => (
     // <h2>Teste cris</h2>
     <div className='app'>
         <Header></Header>
-        <Nav></Nav>
         <Main></Main>
         <Footer></Footer>
     </div>
