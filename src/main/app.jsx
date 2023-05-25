0bdba17067a5c1ca5d34a2cf4a8ce77452a26a3d
Cristiane Nazareth <cristianenazareth24@gmail.com>
2023-05-25 19:50:18 -0300

    update main

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,18 +1,37 @@
 import './app.css'
 import React from 'react'
 
-import {Header} from '../components/template/header'
-import {Main} from '../components/template/main'
-import {Footer} from '../components/template/footer'
+import Header from '../components/header/header.jsx'
+import TextLogin from '../components/textLogin/textLogin'
+import AcessLogin from '../components/acessLogin/acessLogin'
+import Nav from '../components/nav/nav'
+import { Button } from '../components/Button'
+import Footer from '../components/footer/footer'
 
+// import {IconUrl} from '../components/iconUrl/iconUrl'
+// export const App = props => (
+//     <div className='app'>
+//         <Header></Header>
+//         <Main></Main>
+//         <Footer></Footer>
+//     </div>
+// )
 
-
-export const App = props => (
-    // <h2>Teste cris</h2>
+export const App = () => (
     <div className='app'>
         <Header></Header>
-        <Main></Main>
-        <Footer></Footer>
-    </div>
+        <main className='content'>
+            <Nav></Nav>
 
+            <AcessLogin></AcessLogin>
+
+
+
+            <TextLogin></TextLogin>
+        </main>
+        <Footer></Footer>
+
+    </div>
 )
+
+export default App;
